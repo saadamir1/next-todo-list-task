85e15a5118b6d13b81183b7ed63c30eb53c5c25f
saadamir1 <saadamir070@gmail.com>
2025-05-07 20:50:36 +0500

    Add custom app title in _document.js

---
+++ src/pages/_document.js
@@ -0,0 +1,20 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+class MyDocument extends Document {
+    render() {
+        return (
+            <Html lang="en">
+                <Head>
+                    <title>My Tasks App</title>
+                    <meta name="description" content="A simple task manager app" />
+                </Head>
+                <body>
+                    <Main />
+                    <NextScript />
+                </body>
+            </Html>
+        );
+    }
+}
+
+export default MyDocument;
